8bdaacfe59aad0b3bc01d077def43cca383191fb
Umang #Dinjan <umang@dinjaninfotech.com>
2024-01-26 15:54:34 +0530

    final bug fixed

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -82,9 +82,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "ae4b54d347617868b56b12509cd6b29d
 "icons/Icon-512.png": "faee57212a06ec238d0d21beeb504da2",
 "icons/Icon-maskable-192.png": "cc8d3cdd186ac22d0b10658210db2ccb",
 "icons/Icon-maskable-512.png": "faee57212a06ec238d0d21beeb504da2",
-"index.html": "251489c80d181ddc98069d6b147dd030",
-"/": "251489c80d181ddc98069d6b147dd030",
-"main.dart.js": "83c2f90b918d7e3002231dbb37637865",
+"index.html": "908316f6559709c6357e8090517cc465",
+"/": "908316f6559709c6357e8090517cc465",
+"main.dart.js": "956ce138fad1ec25c82bbad22918c744",
 "manifest.json": "247aec96ad4ffa5647b5e954e164f247",
 "version.json": "cb648f3438e620a93558e1cee82a83cd"};
 // The application shell files that are downloaded before a service worker can
